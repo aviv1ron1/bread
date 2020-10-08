b4e919b471c7def88383945f0b3b6e4c23def8e1
Aviv Ron <aviv1ron1@gmail.com>
2020-10-08 14:42:43 +0300

    bug fixes - remove db name from xreation script

--- db.sql
+++ db.sql
@@ -1,228 +1,204 @@
-/****** Object:  Table [dbo].[Matcon]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Matcon](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserId] [int] NOT NULL,
-	[Name] [nvarchar](200) NOT NULL,
-	[Rating] [float] NOT NULL,
-	[Data] [nvarchar](max) NOT NULL,
- CONSTRAINT [PK_Matcon] PRIMARY KEY CLUSTERED 
+/****** Object:  Table `Matcon`    Script Date: 04/08/2020 17:26:42 ******/
+use `bread`;
+
+CREATE TABLE `matcon`(
+	`Id` int AUTO_INCREMENT NOT NULL,
+	`UserId` int NOT NULL,
+	`Name` varchar(200) NOT NULL,
+	`Rating` double NOT NULL,
+	`Data` Text NOT NULL,
+ CONSTRAINT `PK_Matcon` PRIMARY KEY 
 (
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[MatconRating]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[MatconRating](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[MatconId] [int] NOT NULL,
-	[UserId] [int] NOT NULL,
-	[Timestamp] [datetime] NOT NULL,
-	[Rating] [int] NOT NULL,
-	[Data] [nvarchar](max) NOT NULL,
- CONSTRAINT [PK_MatconRating] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[MatconTag]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[MatconTag](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[MatconId] [int] NOT NULL,
-	[Tag] [nvarchar](100) NOT NULL,
- CONSTRAINT [PK_MatconTag] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[PreRegister]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[PreRegister](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Email] [nvarchar](320) NOT NULL,
-	[Token] [char](100) NOT NULL,
-	[Timestamp] [datetime] NOT NULL,
-	[EmailValidated] [bit] NOT NULL,
- CONSTRAINT [PK_PreRegister] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[User]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[User](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Email] [nvarchar](320) NOT NULL,
-	[Data] [nvarchar](max) NOT NULL,
- CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[UserAuth]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[UserAuth](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserId] [int] NOT NULL,
-	[Token] [varchar](100) NOT NULL,
-	[Data] [nvarchar](max) NOT NULL,
- CONSTRAINT [PK_UserAuth] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[PreRegister] ADD  CONSTRAINT [DF_PreRegister_EmailValidated]  DEFAULT ((0)) FOR [EmailValidated]
-GO
-ALTER TABLE [dbo].[Matcon]  WITH CHECK ADD  CONSTRAINT [FK_Matcon_User] FOREIGN KEY([UserId])
-REFERENCES [dbo].[User] ([Id])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[Matcon] CHECK CONSTRAINT [FK_Matcon_User]
-GO
-ALTER TABLE [dbo].[MatconRating]  WITH CHECK ADD  CONSTRAINT [FK_MatconRating_Matcon] FOREIGN KEY([MatconId])
-REFERENCES [dbo].[Matcon] ([Id])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[MatconRating] CHECK CONSTRAINT [FK_MatconRating_Matcon]
-GO
-ALTER TABLE [dbo].[MatconRating]  WITH CHECK ADD  CONSTRAINT [FK_MatconRating_User] FOREIGN KEY([UserId])
-REFERENCES [dbo].[User] ([Id])
-GO
-ALTER TABLE [dbo].[MatconRating] CHECK CONSTRAINT [FK_MatconRating_User]
-GO
-ALTER TABLE [dbo].[MatconTag]  WITH CHECK ADD  CONSTRAINT [FK_MatconTag_Matcon] FOREIGN KEY([MatconId])
-REFERENCES [dbo].[Matcon] ([Id])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[MatconTag] CHECK CONSTRAINT [FK_MatconTag_Matcon]
-GO
-ALTER TABLE [dbo].[UserAuth]  WITH CHECK ADD  CONSTRAINT [FK_UserAuth_User] FOREIGN KEY([UserId])
-REFERENCES [dbo].[User] ([Id])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[UserAuth] CHECK CONSTRAINT [FK_UserAuth_User]
-GO
-/****** Object:  StoredProcedure [dbo].[DeleteOldPreRegisters]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[DeleteOldPreRegisters]
-(
-    -- Add the parameters for the stored procedure here
-    @ExpirationDays int = 3
+	`Id` ASC
 )
-AS
-BEGIN
-    -- SET NOCOUNT ON added to prevent extra result sets FROM
-    -- interfering with SELECT statements.
-    SET NOCOUNT ON
-
-    -- Insert statements for procedure here
-    DELETE FROM PreRegister WHERE [Timestamp] < DATEADD(day, @ExpirationDays*-1, GETDATE())
-	
-	SELECT @@ROWCOUNT as [Deleted]
-END
-GO
-/****** Object:  StoredProcedure [dbo].[GetMatcon]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE PROCEDURE [dbo].[GetMatcon]
-(
-    @Id int = 0
 )
-AS
+;
+
+
+/****** Object:  Table `MatconRating`    Script Date: 04/08/2020 17:26:42 ******/
+
+
+CREATE TABLE `matconrating`(
+	`Id` int AUTO_INCREMENT NOT NULL,
+	`MatconId` int NOT NULL,
+	`UserId` int NOT NULL,
+	`Timestamp` datetime NOT NULL,
+	`Rating` int NOT NULL,
+	`Data` Text NOT NULL,
+ CONSTRAINT `PK_MatconRating` PRIMARY KEY 
+(
+	`Id` ASC
+)
+)
+;
+/****** Object:  Table `tag`    Script Date: 04/08/2020 17:26:42 ******/
+
+CREATE TABLE `tag` (
+  `id` INT NOT NULL,
+  `tag` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`));
+
+
+/****** Object:  Table `MatconTag`    Script Date: 04/08/2020 17:26:42 ******/
+CREATE TABLE `matcontag`(
+	`Id` int AUTO_INCREMENT NOT NULL,
+	`MatconId` int NOT NULL,
+	`TagId` int NOT NULL,
+ CONSTRAINT `PK_MatconTag` PRIMARY KEY 
+(
+	`Id` ASC
+)
+)
+;
+/****** Object:  Table `PreRegister`    Script Date: 04/08/2020 17:26:42 ******/
+
+
+CREATE TABLE `preregister`(
+	`Id` int AUTO_INCREMENT NOT NULL,
+	`Email` varchar(320) NOT NULL,
+	`Token` varchar(100) NOT NULL,
+	`Timestamp` datetime NOT NULL,
+	`EmailValidated` tinyint NOT NULL,
+ CONSTRAINT `PK_PreRegister` PRIMARY KEY 
+(
+	`Id` ASC
+)
+)
+;
+/****** Object:  Table `User`    Script Date: 04/08/2020 17:26:42 ******/
+
+
+CREATE TABLE `user`(
+	`Id` int AUTO_INCREMENT NOT NULL,
+	`Email` varchar(320) NOT NULL,
+	`Data` Text NOT NULL,
+ CONSTRAINT `PK_User` PRIMARY KEY 
+(
+	`Id` ASC
+)
+)
+;
+/****** Object:  Table `UserAuth`    Script Date: 04/08/2020 17:26:42 ******/
+
+
+CREATE TABLE `userauth`(
+	`Id` int AUTO_INCREMENT NOT NULL,
+	`UserId` int NOT NULL,
+	`Token` varchar(100) NOT NULL,
+	`Data` Text NOT NULL,
+ CONSTRAINT `PK_UserAuth` PRIMARY KEY 
+(
+	`Id` ASC
+)
+)
+;
+
+
+ALTER TABLE `matcon` 
+ADD CONSTRAINT `matcon_userid_user_id`
+  FOREIGN KEY (`UserId`)
+  REFERENCES `user` (`Id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `matconrating` 
+ADD CONSTRAINT `matconrating_matconid_matcon_id`
+  FOREIGN KEY (`MatconId`)
+  REFERENCES `matcon` (`Id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `matconrating` 
+ADD CONSTRAINT `matconrating_matconid_user_id`
+  FOREIGN KEY (`UserId`)
+  REFERENCES `user` (`Id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `matcontag` 
+ADD CONSTRAINT `matcontag_matconid_matcon_id`
+  FOREIGN KEY (`MatconId`)
+  REFERENCES `matcon` (`Id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `matcontag` 
+ADD CONSTRAINT `matcontag_tagid_tag_id`
+  FOREIGN KEY (`TagId`)
+  REFERENCES `tag` (`Id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `userauth` 
+ADD CONSTRAINT `userauth_userid_user_id`
+  FOREIGN KEY (`UserId`)
+  REFERENCES `user` (`Id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+
+/****** Object:  StoredProcedure `DeleteOldPreRegisters`    Script Date: 04/08/2020 17:26:42 ******/
+
+
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE  `DeleteOldPreRegisters`
+(
+	IN ExpirationDays int
+)
 BEGIN
-    SET NOCOUNT ON
-
-	SELECT * FROM [Matcon] WHERE [Id] = @Id
-
-	SELECT [User].[Data] FROM [User] INNER JOIN [Matcon] ON [Matcon].[UserId] = [User].[Id] WHERE [Matcon].[Id] = @Id
-
-	SELECT AVG([MatconRating].[Rating]) AS [Rating], COUNT([MatconRating].[Id]) AS [RatingCount] FROM [MatconRating] WHERE [MatconRating].[MatconId] = @Id
-
-	SELECT [MatconTag].[Id], [MatconTag].[Tag] FROM [MatconTag] WHERE [MatconTag].[MatconId] = @Id
-
+	DELETE FROM preregister WHERE `Timestamp` < DATE_SUB(NOW(), INTERVAL ExpirationDays DAY);
     
-END
-GO
+	SELECT ROWCOUNT() as `Deleted`;
+END$$
+/****** Object:  StoredProcedure `GetMatcon`    Script Date: 04/08/2020 17:26:42 ******/
 
-/****** Object:  StoredProcedure [dbo].[AddReview]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
-CREATE PROCEDURE [dbo].[AddReview]
-(
-    @MatconId INT,
-	@Rating INT,
-	@UserId INT,
-	@Data NVARCHAR(MAX)
-)
-AS
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE  `GetMatcon`(IN Id int(0))
 BEGIN
-    SET NOCOUNT ON
+	SELECT * FROM `matcon` WHERE `Id` = Id;
 
-	DELETE FROM [MatconRating] WHERE [MatconId] = @MatconId AND [UserId] = @UserId
+	SELECT `user`.`Data` FROM `user` INNER JOIN `matcon` ON `matcon`.`UserId` = `user`.`Id` WHERE `matcon`.`Id` = Id;
 
-	INSERT INTO [MatconRating] VALUES (@MatconId, @UserId, GETDATE(), @Rating, @Data);
+	SELECT AVG(`matconrating`.`Rating`) AS `Rating`, COUNT(`matconrating`.`Id`) AS `RatingCount` FROM `matconrating` WHERE `matconrating`.`MatconId` = Id;
 
-	UPDATE [Matcon] SET [Rating] = (SELECT AVG(Cast([MatconRating].[Rating] as Float)) FROM [MatconRating] WHERE [MatconId] = @MatconId)
+	SELECT `tag`.`Id`, `tag`.`Tag` FROM `matcontag` INNER JOIN `tag` on `matcontag`.`TagId` = `tag`.`Id`WHERE `matcontag`.`MatconId` = Id;
+END$$
 
-END
-GO
+/****** Object:  StoredProcedure `AddReview`    Script Date: 04/08/2020 17:26:42 ******/
 
-/****** Object:  StoredProcedure [dbo].[DeleteReview]    Script Date: 04/08/2020 17:26:42 ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
 
-CREATE PROCEDURE [dbo].[DeleteReview]
-(
-    @Id INT
+CREATE DEFINER=`root`@`localhost` PROCEDURE  `AddReview`(
+	IN MatconId int,
+    IN Rating int,
+    IN UserId int,
+    IN `Data` Text
 )
-AS
 BEGIN
-    SET NOCOUNT ON
+	DELETE FROM `matconrating` WHERE `MatconId` = MatconId AND `UserId` = UserId;
 
-	DECLARE @MatconId INT;
+	INSERT INTO `matconrating` VALUES (MatconId, UserId, NOW(), Rating, `Data`);
 
-	SET @MatconId = (SELECT [MatconId] FROM [MatconRating] WHERE [Id] = @Id)
+	UPDATE `matcon` SET `Rating` = (SELECT AVG(Cast(`matconrating`.`Rating` as Float)) FROM `matconrating` WHERE `MatconId` = MatconId);
+END$$
 
-	DELETE FROM [MatconRating] WHERE [Id] = @Id
+/****** Object:  StoredProcedure `DeleteReview`    Script Date: 04/08/2020 17:26:42 ******/
 
-	UPDATE [Matcon] SET [Rating] = ISNULL((SELECT AVG(Cast([MatconRating].[Rating] as Float)) FROM [MatconRating] WHERE [MatconId] = @MatconId), 0)
 
-END
-GO
+CREATE DEFINER=`root`@`localhost` PROCEDURE  `DeleteReview`(
+	IN Id int
+)
+BEGIN
+	DECLARE MatconId INT;
+
+	SET MatconId = (SELECT `MatconId` FROM `matconrating` WHERE `Id` = Id);
+
+	DELETE FROM `matconrating` WHERE `Id` = Id;
+
+	UPDATE `matcon` SET `Rating` = IFNULL((SELECT AVG(Cast(`matconrating`.`Rating` as Float)) FROM `matconrating` WHERE `MatconId` = MatconId), 0);
+
+END$$
+
+DELIMITER ;
